e0a235a86cad5edff2235c0e394ce879ea1d42fa
naftysik <naftysikmilani@gmail.com>
2023-05-03 22:13:23 +0200

    Added a bootstrap

--- src/App.js
+++ src/App.js
@@ -3,5 +3,38 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App by Naftysik</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nataly-tsalko-5a1440103//"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tsalko Natalka
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/naftysik/React-App-7745"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://rainbow-klepon-8d5d2b.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
